0b14e8cceee02e19618518e23c273ca96b25997c
Read Xia <readxia360@gmail.com>
2023-03-13 20:21:35 -0500

    Update solution.js

--- Assessment_1/q5/solution.js
+++ Assessment_1/q5/solution.js
@@ -1,8 +1,12 @@
 function findHay(arr) {
+    //loop through array with for loop
     for (let i = 0; i < arr.length; i++) {
+        //check if array element === keyword 'hay'
         if (arr[i] === 'hay') {
+            //return concatenated phrase with index location
             return 'found the word at position ' + i
         }
     }
+    //return exit if no 'hay' was found in arr
     return 'did not find the word "hay"'
 }
