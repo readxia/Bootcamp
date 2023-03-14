9b9401fcbd91131343f7f050b721116ecb0bbadf
Read Xia <readxia360@gmail.com>
2023-03-13 20:16:57 -0500

    added comments
    added comments

--- Assessment_1/q2/solution.js
+++ Assessment_1/q2/solution.js
@@ -1,7 +1,10 @@
 function negativeNum(n) {
+    //checks if n is negative
     if (n <= 0) {
         return n
     }
+    
+    //if n is positive, we return negative of N
     else {
         return n * -1
     }
