f78670255e048dfbb54201ec777a759e867311e4
readxia <readxia360@gmail.com>
2023-07-08 17:25:05 -0500

    added comments to the PRACTICAL portion

--- Final/D_PRACTICAL/final_readxia/index.js
+++ Final/D_PRACTICAL/final_readxia/index.js
@@ -2,15 +2,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// HOME ROUTE FOR TRENDING PAGE
 app.get('/', async (req, res)=> {
     let gifList = []
+
+    //API FETCH FOR TRENDING
     const trendingGifs = async () => {
         const baseUrl = "http://api.giphy.com/v1/gifs/trending"
+        // SUPER SECURE API KEY
         const key = 'AKmJ3QDsFpbU3s6RhD8A24uNiV6SE8Wb'
         let endpoint = `${baseUrl}?api_key=${key}&limit=25`
         const response = await fetch(endpoint)
         if (response.ok) {
             let gifdata = await response.json()
+            //PUT ALL THE DOWNSIZED GIF URLS INTO A LIST
             gifList = gifdata.data.map((obj) => obj.images.downsized.url)
 
         }
@@ -21,17 +26,24 @@ app.get('/', async (req, res)=> {
     }
     await trendingGifs()
     
+    // RENDER
     res.render('home.ejs', {gifList: gifList})
 })
 
+// SEARCH ROUTE
 app.get('/searchForGifs/:page', async (req, res) => {
     let gifList = []
+    // Grab the search term used
     const {search} = req.query
     let pagination = {}
 
+    //API fetch for searching gifs
     const searchGifs = async () => {
         const baseUrl = "http://api.giphy.com/v1/gifs/search"
+        //super secure API key
         const key = 'AKmJ3QDsFpbU3s6RhD8A24uNiV6SE8Wb'
+
+        //creating the 'pagination' with offset set to 25 gifs per page
         let offset = parseInt(req.params.page)
         offset *= 25
 
@@ -39,6 +51,7 @@ app.get('/searchForGifs/:page', async (req, res) => {
         const response = await fetch(endpoint)
         if (response.ok) {
             let gifdata = await response.json()
+            //put all the downsized urls into a list
             gifList = gifdata.data.map((obj) => obj.images.downsized.url)
             pagination = gifdata.pagination
 
@@ -49,6 +62,7 @@ app.get('/searchForGifs/:page', async (req, res) => {
         }
     }
     await searchGifs()
+    //send information about what search term was used, and what page we are on to EJS
     res.render('search.ejs', {gifList: gifList, searchTerm: search, pagination: pagination, page: parseInt(req.params.page)})
 })
 
